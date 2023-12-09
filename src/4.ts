e8c3c2843cad70b5da638fd184dca7eb6b796b17
Chalaya22 <chalayaolga22@gmail.com>
2023-12-09 02:14:07 +0200

    comit 3

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,51 @@
+class Key {
+  private signature: number;
+
+  getSignature(): number {
+    return (this.signature = Math.random());
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return key;
+  }
+}
+
+abstract class House {
+  constructor(
+    protected door: boolean,
+    protected key: Key,
+    protected tenants: Person[]
+  ) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+  comeIn(person: Person): void {
+    this.door === true && this.tenants.push(person);
+  }
+  abstract OpenDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  OpenDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
